250abda797cb72c19a9f5de84bb322cb57d59e1e
joezdeng <joe.z.deng@gmail.com>
2015-01-06 10:18:55 -0600

    init commit for repo2

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,60 @@
 (function() {
-	var app = angular.module('pursuits'), []);
+	var app = angular.module('prsts', []);
 
 	app.controller('CategoryController', function() {
 		this.cats = categories;
 	});
 
-	
+	app.controller('ArticleController', function() {
+		this.arts = articles;
+	});
+
+	app.controller('SuggestedController', function() {
+		this.sugJobs = suggestedJobs;
+	});
+
+	app.controller('UserController', function() {
+		this.user = user;
+	});
+
+	app.controller('PursuitsController', function() {
+		this.currentPursuits = pursuits;
+	});
+
+	app.controller('AddOptionController', function() {
+		this.option = {};
+
+		this.addOption = function() {
+			var category = this.option.category;
+
+			for(i = 0; i < categories.length; i++) {
+				switch(category) {
+					case categories[i].name:
+						categories[i].options.push(this.option);
+						break;
+				}
+			}
+
+			this.option = {};
+		};
+	});
+
+	app.controller('AddCatController', function() {
+		this.category = {};
+
+		this.addCat = function() {
+			categories.push(this.category);
+
+			this.category = {};
+
+			var wrapWidth = document.getElementById('category-wrap').scrollWidth;
+			var windowWidth = $(window).width()
+			var maxScrollX = wrapWidth - windowWidth;
+			$('#category-wrap').scrollLeft(maxScrollX);
+
+		};
+	});
+
 
 	var categories = [
 		{
@@ -15,42 +64,49 @@
 					coName:'Company A',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company B',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company C',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company D',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company E',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company F',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company G',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				}
 			]
@@ -63,24 +119,28 @@
 					coName:'Company H',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company I',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company J',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company K',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				}
 			]
@@ -93,42 +153,49 @@
 					coName:'Company L',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company M',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company N',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company O',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company P',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company Q',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company R',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				}
 			]
@@ -141,18 +208,21 @@
 					coName:'Company S',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company T',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company U',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				}
 			]
@@ -165,12 +235,14 @@
 					coName:'Company V',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				},
 				{
 					coName:'Company W',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				}
 			]
@@ -183,10 +255,96 @@
 					coName:'Company X',
 					coLocation:'City, State',
 					jobTitle:'Job Title',
+					notes:'Brief Description',
 					image:''
 				}
 			]
 		}
 	];
+
+	var articles = [
+		{
+			title:'Article A',
+			href:''
+		},
+		{
+			title:'Article B',
+			href:''
+		},
+		{
+			title:'Article C',
+			href:''
+		},
+		{
+			title:'Article D',
+			href:''
+		},
+		{
+			title:'Article E',
+			href:''
+		},
+		{
+			title:'Article F',
+			href:''
+		},
+		{
+			title:'Article G',
+			href:''
+		},
+	];
+
+	var suggestedJobs = [
+		{
+			coName:'Company AA',
+			coLocation:'City, State',
+			jobTitle:'Job Title',
+			image:''
+		},
+		{
+			coName:'Company BB',
+			coLocation:'City, State',
+			jobTitle:'Job Title',
+			image:''
+		},
+		{
+			coName:'Company CC',
+			coLocation:'City, State',
+			jobTitle:'Job Title',
+			image:''
+		},
+		{
+			coName:'Company DD',
+			coLocation:'City, State',
+			jobTitle:'Job Title',
+			image:''
+		},
+		{
+			coName:'Company EE',
+			coLocation:'City, State',
+			jobTitle:'Job Title',
+			image:''
+		},
+		{
+			coName:'Company FF',
+			coLocation:'City, State',
+			jobTitle:'Job Title',
+			image:''
+		},
+	];
+
+	var user = {
+		firstName:'John',
+		lastName:'Doe',
+		email:'johncreative@gmail.com',
+		image:''
+	};
+
+	var pursuits = [
+		'Full-Time Web Development',
+		'Part-Time Freelance',
+		'Pro Bono Work'
+	];
+
+
 
 })();
--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,4 +1,56 @@
-$('#suggested-button, .fly-menu p').click(function () {
-        $('.fly-menu').toggleClass('hidden');
+//Suggested Sidebar toggle
+$('#suggested-button, .fly-menu p').click(function() {
+	$('.fly-menu').toggleClass('hidden');
 
     });
+
+$('header, main').click(function() {
+	$('.fly-menu').addClass('hidden');
+
+});
+
+
+//Add Option Menu toggle
+$('#add-option').click(function() {
+	$('.overlay, #add-option-form').fadeIn(function() {
+		$(this).removeClass('hidden');
+	});
+
+});
+
+//Add Category Menu toggle
+$('#add-cat').click(function() {
+	$('.overlay, #add-cat-form').fadeIn(function() {
+		$(this).removeClass('hidden');
+	});
+
+});
+
+//Overlay toggle
+$('.overlay').click(function() {
+	$('.overlay, .popup').fadeOut(function() {
+		$(this).addClass('hidden');
+	});
+
+});
+
+$(document).keyup(function(e) {
+	if(e.keyCode == 27) {
+		$('.overlay, .popup').fadeOut(function() {
+			$(this).addClass('hidden');
+		});
+	}
+});
+
+//Details Button toggle
+$('.option img').click(function() {
+	console.log('clicked');
+	$('.details-button').toggleClass('hidden');
+
+});
+
+$('.option').hover(function() {
+	$('.details-button').removeClass('hidden');
+}, function() {
+	$('.details-button').addClass('hidden');
+});
